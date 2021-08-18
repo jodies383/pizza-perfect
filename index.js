351c6b3cdac45285c0f90667dc2690aa73ab6f30
avermeulen <andre.vermeulen@gmail.com>
2021-08-18 14:00:41 +0200

    :-x

--- index.js
+++ index.js
@@ -10,21 +10,6 @@ app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.static('public'));
 
-app.get('/', function(req, res){
-	res.render('index');
-});
-
-app.get('/pizzas', function(req, res) {
-	res.render("pizza/index");
-});
-
-app.post('/pizza/add', function(req, res) {
-	res.redirect("/pizzas");
-});
-
-app.get('/pizza/add', function(req, res){
-	res.render('pizza/add')
-});
 
 const PORT = process.env.PORT || 7008;
 app.listen(PORT, function(){
